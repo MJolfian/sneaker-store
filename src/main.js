147cae1ade58574a03ff51c83ac5bca4bcbbc433
MJolfian <mohammadjavadolfian@gmail.com>
2026-08-14 15:26:00 +0330

    add page 2/5 of onboarding section of figma

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import './style.css'
-const containerTagOfOnboardingPage=document.getElementById('onboarding-container');
+const slider=document.getElementById('slider');
+const onboardingPage=document.getElementById('onboarding-page');
+const sliderContainer=document.getElementsByTagName('main')[0];
+let secondPage=document.getElementById('p2');
 
-//setTimeout(()=>{
-//	containerTagOfOnboardingPage.innerHTML='';
-//},2500)
+let currentPage=1;
+
+function goToNextPage(pageNumber){
+	slider.style.transform =`translateX(-${currentPage*100}%)`;
+	currentPage++;
+}
+
+setTimeout(()=>{
+	goToNextPage(currentPage);
+},3000)
+
+setTimeout(()=>{
+	secondPage.addEventListener('click',()=>{
+		goToNextPage(currentPage);
+})},5000)
+
+
+
+let viewportWidth=document.getElementsByClassName('vw')[0];
+console.log(viewportWidth)
+viewportWidth.textContent=`viewport width:${window.innerWidth}px\nviewport height:${window.innerHeight}px`
