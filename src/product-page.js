b128a7a022bdd523723dfda87a33f859b99cc61c
MJolfian <mohammadjavadolfian@gmail.com>
2026-09-11 11:40:37 +0330

    create cart item based on ui

--- src/product-page.js
+++ src/product-page.js
@@ -1,1 +1,38 @@
 // JavaScript Document
+import {getShoeItem} from '../apis/shoe-list.js';
+import {homeErrorHandler} from './home-error-handler.js';
+
+const addToCartSvg = document.getElementById('add-to-cart-icon');
+const backIcon = document.getElementById('back-icon');
+const productImg = document.getElementById('product-img');
+const productName = document.getElementById('product-name');
+const productPrice = document.getElementById('product-price');
+
+addToCartSvg.innerHTML = `<path d="M7 8.2C6.2 8.5 5.7 9.2 5.6 10.1L4.7 17.8C4.5 19.5 5.8 20.8 7.5 20.8H16.5C18.2 20.8 19.5 19.5 19.3 17.8L18.4 10.1C18.3 9.2 17.8 8.5 17 8.2C15.7 7.7 14.3 7.5 12 7.5C9.7 7.5 8.3 7.7 7 8.2Z"
+    			fill="currentColor"/>
+				<path d="M8.5 8C8.5 5.5 9.7 3.8 12 3.8C14.3 3.8 15.5 5.5 15.5 8" stroke="currentColor" stroke-width="1.8" stroke-linecap="round"/>
+				<circle cx="9.5" cy="12" r="0.9" fill="black"/>
+  				<circle cx="14.5" cy="12" r="0.9" fill="black"/>`;
+
+backIcon.addEventListener('click', () => history.back());
+
+const params = new URLSearchParams(location.search);
+const pid = params.get('pid');
+
+async function getShoe(pid){
+	try{
+		const res = await getShoeItem(pid);
+		showProduct(res);
+		console.log(res)
+	}catch(error){
+		homeErrorHandler(error);
+	}
+	
+}
+getShoe(pid);
+
+const showProduct = ({imageURL, name, price}) => {
+	productImg.src = imageURL;
+	productName.innerText = name;
+	productPrice.append(price);
+}
--- src/search.js
+++ src/search.js
@@ -10,9 +10,13 @@ let numOfFoundResults = document.getElementById('num-of-found-results');
 const paginationHintText = document.getElementById('pagination-hint');
 let containerOfProductsSection = document.getElementById('container-of-products-section');
 
+if(!localStorage.getItem('shoe-app-token')) location.href = '/login';
 
-const makeSearchSvgDefaultState = () => searchSvg.innerHTML=`<path d="M11.742 10.3421C12.7103 9.02083 13.144 7.38264 12.9563 5.7553C12.7687 4.12796 11.9735 2.63149 10.7298 1.56528C9.48616 0.499068 7.88579 -0.0582491 6.24888 0.00482408C4.61197 0.0678972 3.05923 0.746709 1.90131 1.90545C0.743395 3.0642 0.0656939 4.61742 0.00379204 6.25438C-0.0581098 7.89134 0.500353 9.49131 1.56745 10.7342C2.63455 11.9771 4.13159 12.7712 5.75906 12.9577C7.38654 13.1442 9.02442 12.7094 10.345 11.7401H10.344C10.374 11.7801 10.406 11.8181 10.442 11.8551L14.292 15.7051C14.4796 15.8928 14.7339 15.9983 14.9992 15.9983C15.2645 15.9984 15.5189 15.8932 15.7065 15.7056C15.8942 15.5181 15.9997 15.2638 15.9997 14.9985C15.9998 14.7332 15.8946 14.4788 15.707 14.2911L11.857 10.4411C11.8213 10.405 11.7828 10.3715 11.742 10.3411V10.3421ZM12 6.49815C12 7.22042 11.8578 7.93562 11.5814 8.60291C11.305 9.2702 10.8999 9.87651 10.3891 10.3872C9.87841 10.898 9.27209 11.3031 8.6048 11.5795C7.93751 11.8559 7.22231 11.9981 6.50004 11.9981C5.77777 11.9981 5.06258 11.8559 4.39528 11.5795C3.72799 11.3031 3.12168 10.898 2.61096 10.3872C2.10023 9.87651 1.69511 9.2702 1.41871 8.60291C1.14231 7.93562 1.00004 7.22042 1.00004 6.49815C1.00004 5.03946 1.57951 3.64051 2.61096 2.60906C3.64241 1.57761 5.04135 0.998147 6.50004 0.998147C7.95873 0.998147 9.35768 1.57761 10.3891 2.60906C11.4206 3.64051 12 5.03946 12 6.49815Z" fill="currentColor"/>
-`;
+
+const makeSearchSvgDefaultState = () =>{
+	if(!searchSvg) return; // for solve export problem
+	searchSvg.innerHTML=`<path d="M11.742 10.3421C12.7103 9.02083 13.144 7.38264 12.9563 5.7553C12.7687 4.12796 11.9735 2.63149 10.7298 1.56528C9.48616 0.499068 7.88579 -0.0582491 6.24888 0.00482408C4.61197 0.0678972 3.05923 0.746709 1.90131 1.90545C0.743395 3.0642 0.0656939 4.61742 0.00379204 6.25438C-0.0581098 7.89134 0.500353 9.49131 1.56745 10.7342C2.63455 11.9771 4.13159 12.7712 5.75906 12.9577C7.38654 13.1442 9.02442 12.7094 10.345 11.7401H10.344C10.374 11.7801 10.406 11.8181 10.442 11.8551L14.292 15.7051C14.4796 15.8928 14.7339 15.9983 14.9992 15.9983C15.2645 15.9984 15.5189 15.8932 15.7065 15.7056C15.8942 15.5181 15.9997 15.2638 15.9997 14.9985C15.9998 14.7332 15.8946 14.4788 15.707 14.2911L11.857 10.4411C11.8213 10.405 11.7828 10.3715 11.742 10.3411V10.3421ZM12 6.49815C12 7.22042 11.8578 7.93562 11.5814 8.60291C11.305 9.2702 10.8999 9.87651 10.3891 10.3872C9.87841 10.898 9.27209 11.3031 8.6048 11.5795C7.93751 11.8559 7.22231 11.9981 6.50004 11.9981C5.77777 11.9981 5.06258 11.8559 4.39528 11.5795C3.72799 11.3031 3.12168 10.898 2.61096 10.3872C2.10023 9.87651 1.69511 9.2702 1.41871 8.60291C1.14231 7.93562 1.00004 7.22042 1.00004 6.49815C1.00004 5.03946 1.57951 3.64051 2.61096 2.60906C3.64241 1.57761 5.04135 0.998147 6.50004 0.998147C7.95873 0.998147 9.35768 1.57761 10.3891 2.60906C11.4206 3.64051 12 5.03946 12 6.49815Z" fill="currentColor"/>`;
+}
 makeSearchSvgDefaultState();
 
 const params = new URLSearchParams(location.search);
@@ -78,6 +82,7 @@ const getSneakersBasedOnSearchWords = async () => {
 function showRecievedSneakers(shoesArray){
 		searchWordSpan.textContent = `Results for "${searchValue}"`;
 		numOfFoundResults.innerText = totalFoundItem + ' found';
+		
 	if(shoesArray.length === 0){
 		searchInput.blur();
 		return notFoundImg.classList.remove('hidden');
@@ -94,7 +99,8 @@ function showRecievedSneakers(shoesArray){
 		img.className = 'rounded-3xl aspect-square';
 		img.src = item.imageURL;
 		img.alt = item.brand;
-		div.classList = 'product-box flex flex-col';
+		div.classList = 'product-box flex flex-col cursor-pointer';
+		div.dataset.pid = item.pid; // or: div.setAttribute('data-pid', item.pid);
 		innerDiv.classList = 'grow flex flex-col justify-around';
 		h3.classList = 'font-bold text-[1.125rem]/none tracking-[-4%] text-title mt-3 mb-2 truncate sm:text-wrap sm:line-clamp-2';
 		h3.textContent = item.name;
@@ -105,6 +111,13 @@ function showRecievedSneakers(shoesArray){
 		innerDiv.append(h3, p);
 	});
 }
+
+containerOfProductsSection.addEventListener('click', (event) => {
+		if(!event.target.closest('.product-box')) return;
+		const productBox = event.target.closest('.product-box');
+		const pid = productBox.dataset.pid;
+		location.href = `/product-page?pid=${pid}`;
+	})
 
 let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
